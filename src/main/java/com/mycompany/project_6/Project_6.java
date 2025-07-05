4f3714edff7516eb823944a1547ce8609e322208
USER <USER@DESKTOP-1646F9I>
2025-07-05 12:32:08 +0700

    pemula

---
+++ src/main/java/com/mycompany/project_6/Project_6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project_6;
+
+/**
+ *
+ * @author USER
+ */
+public class Project_6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
